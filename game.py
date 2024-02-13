e44c3a51e3222fc6ca59b7114444490f9a736812
Agent Andrew <agentandrewminecraft@gmail.com>
2024-02-13 15:31:06 -0600

    moved board selecting logic into board class + simplified code

--- game.py
+++ game.py
@@ -1,35 +1,28 @@
 import pygame
-from board import Board
+from objects.board import Board
+from objects.panel import Panel
 from utils.get_sizes import get_sizes
-from constants import ARROWS, NUMBERS, BLACK, WHITE
+from constants import ARROWS, NUMBERS, WHITE
 
 
 class Game:
     def __init__(self, screen: pygame.surface.Surface) -> None:
         self.active = True
         self.board = Board()
+        self.panel = Panel()
 
         sizes = get_sizes(*screen.get_size())
         self.set_sizes(sizes)
 
     def set_sizes(self, sizes: dict[str, int]) -> None:
         self.__dict__.update(sizes)
+        
         self.board.set_sizes(sizes)
+        self.panel.set_sizes(sizes)
 
     def click(self, x: int, y: int) -> None:
-        # if the cursor is on the board
-        if (
-            x >= self.x_padd
-            and x <= self.x_padd + self.board_size
-            and y >= self.y_padd
-            and y <= self.y_padd + self.board_size
-        ):
-            # find the row and col
-            row = (y - self.y_padd) // self.cell_size
-            col = (x - self.x_padd) // self.cell_size
-
-            # highlight that square
-            self.board.highlighted = [row, col]
+        if self.board.is_over(x, y):
+            self.board.select(x, y)
 
     def press_key(self, key: int) -> None:
         for arrow in ARROWS:
@@ -45,14 +38,4 @@ class Game:
         screen.fill(WHITE)
 
         self.board.draw(screen)
-
-        self.panel_start = self.x_padd + self.board_size + self.padd
-        self.panel_width = self.cell_size * 2
-
-        # panel border
-        pygame.draw.rect(
-            screen,
-            BLACK,
-            (self.panel_start, self.y_padd, self.panel_width, self.board_size),
-            self.big_line_size,
-        )
+        self.panel.draw(screen)
