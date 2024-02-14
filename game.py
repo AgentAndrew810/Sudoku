6bd3b1531764c7438f825ccc47716abff4a4a3f4
Agent Andrew <agentandrewminecraft@gmail.com>
2024-02-14 15:27:13 -0600

    removed set sizes function

--- game.py
+++ game.py
@@ -1,30 +1,23 @@
 import pygame
 from objects.board import Board
 from objects.panel import Panel
-from utils.get_sizes import get_sizes
 from constants import ARROWS, NUMBERS, WHITE
 
 
 class Game:
     def __init__(self, screen: pygame.surface.Surface) -> None:
         self.active = True
-        self.board = Board()
-        self.panel = Panel()
-
-        sizes = get_sizes(*screen.get_size())
-        self.set_sizes(sizes)
-
-    def set_sizes(self, sizes: dict[str, int]) -> None:
-        self.__dict__.update(sizes)
-
-        self.board.set_sizes(sizes)
-        self.panel.set_sizes(sizes)
+        self.board = Board(screen)
+        self.panel = Panel(screen)
 
     def click(self, x: int, y: int) -> None:
         if self.board.is_over(x, y):
             self.board.select(x, y)
 
-    def press_key(self, key: int) -> None:             
+        if self.panel.is_over(x, y):
+            self.panel.select(x, y)
+
+    def press_key(self, key: int) -> None:
         if key in ARROWS:
             self.board.move(ARROWS[key])
 
