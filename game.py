c2a0741ba33c9396fdbc6d88ab62f102de20a42a
Agent Andrew <agentandrewminecraft@gmail.com>
2024-02-15 12:55:35 -0600

    set sizes of all the objects

--- game.py
+++ game.py
@@ -1,14 +1,21 @@
 import pygame
 from objects.board import Board
 from objects.panel import Panel
+from utils.get_sizes import get_sizes
 from constants import ARROWS, NUMBERS, WHITE
 
 
 class Game:
     def __init__(self, screen: pygame.surface.Surface) -> None:
         self.active = True
-        self.board = Board(screen)
-        self.panel = Panel(screen)
+
+        sizes = get_sizes(*screen.get_size())
+        self.board = Board(sizes)
+        self.panel = Panel(sizes)
+
+    def set_sizes(self, sizes: dict[str:int]) -> None:
+        self.board.set_sizes(sizes)
+        self.panel.set_sizes(sizes)
 
     def click(self, x: int, y: int) -> None:
         if self.board.is_over(x, y):
@@ -16,7 +23,7 @@ class Game:
 
         if self.panel.is_over(x, y):
             button_pressed = self.panel.select(x, y)
-            
+
             if button_pressed == "solve":
                 pass
 
