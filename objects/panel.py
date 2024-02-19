efc7c8d2db9175de63771b6196fd9567bae1b17f
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-19 17:55:31 -0600

    removed is_over method since the button is_over is called anyways

--- objects/panel.py
+++ objects/panel.py
@@ -2,7 +2,7 @@ import pygame
 from objects.button import Button
 from objects.clock import Clock
 from objects.object import Object
-from constants import BLACK, BLUE
+from constants import BLACK, BLUE, YELLOW, RED
 
 
 class Panel(Object):
@@ -15,17 +15,6 @@ class Panel(Object):
             if button.is_over(x, y):
                 return name
 
-    def is_over(self, x: int, y: int) -> bool:
-        if (
-            x >= self.x
-            and y >= self.y
-            and x <= self.x + self.width
-            and y <= self.y + self.height
-        ):
-            return True
-        else:
-            return False
-
     def update(self, new_clock: bool = False) -> None:
         self.x = self.x_padd + self.board_size + self.padd
         self.y = self.y_padd
@@ -34,19 +23,29 @@ class Panel(Object):
 
         # create a new clock or change the position of the clock
         if new_clock:
-            self.clock = Clock(self.x, self.y, self.width, self.height // 3)
+            self.clock = Clock(self.x, self.y, self.width, self.cell_size * 3)
         else:
-            self.clock.change_position(self.x, self.y, self.width, self.height // 3)
+            self.clock.change_position(self.x, self.y, self.width, self.cell_size * 3)
 
         # recreate all the buttons
         self.buttons = {
+            "hint": Button(
+                self.x,
+                self.y + self.cell_size * 3,
+                self.width,
+                self.cell_size * 2,
+                YELLOW,
+            ),
             "solve": Button(
                 self.x,
-                self.y + self.height // 3,
+                self.y + self.cell_size * 5,
                 self.width,
-                self.height // 3,
+                self.cell_size * 2,
                 BLUE,
-            )
+            ),
+            "end": Button(
+                self.x, self.y + self.cell_size * 7, self.width, self.cell_size * 2, RED
+            ),
         }
 
     def draw(self, screen: pygame.surface.Surface) -> None:
