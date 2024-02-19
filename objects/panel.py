2a15db2ef8f24739e2a777f7c7f6f3dcde4e1850
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-19 15:24:01 -0600

    made panel a child of Object, removed set_sizes method and added update method

--- objects/panel.py
+++ objects/panel.py
@@ -1,28 +1,14 @@
 import pygame
 from objects.button import Button
 from objects.clock import Clock
+from objects.object import Object
 from constants import BLACK, BLUE
 
 
-class Panel:
-    def __init__(self, sizes: dict[str:int]) -> None:
-        self.set_sizes(sizes)
-
-    def set_sizes(self, sizes: dict[str, int]) -> None:
-        self.__dict__.update(sizes)
-        
-        self.x = self.x_padd + self.board_size + self.padd
-        self.y = self.y_padd
-        self.width = self.cell_size * 2
-        self.height = self.board_size
-
-        self.clock = Clock(self.x, self.y, self.width, self.height // 3, sizes)
-
-        self.buttons = {
-            "solve": Button(
-                self.x, self.y + self.height // 3, self.width, self.height // 3, BLUE
-            )
-        }
+class Panel(Object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.update(True)
 
     def select(self, x: int, y: int) -> str | None:
         for name, button in self.buttons.items():
@@ -40,12 +26,38 @@ class Panel:
         else:
             return False
 
+    def update(self, new_clock: bool = False) -> None:
+        self.x = self.x_padd + self.board_size + self.padd
+        self.y = self.y_padd
+        self.width = self.cell_size * 2
+        self.height = self.board_size
+
+        # create a new clock or change the position of the clock
+        if new_clock:
+            self.clock = Clock(self.x, self.y, self.width, self.height // 3)
+        else:
+            self.clock.change_position(self.x, self.y, self.width, self.height // 3)
+
+        # recreate all the buttons
+        self.buttons = {
+            "solve": Button(
+                self.x,
+                self.y + self.height // 3,
+                self.width,
+                self.height // 3,
+                BLUE,
+            )
+        }
+
     def draw(self, screen: pygame.surface.Surface) -> None:
+        # draw all the buttons
         for button in self.buttons.values():
             button.draw(screen)
-            
+
+        # draw clock
         self.clock.draw(screen)
 
+        # draw the panel
         pygame.draw.rect(
             screen,
             BLACK,
