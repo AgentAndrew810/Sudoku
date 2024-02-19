95257e0d85b7524288ba33329a785eebd9377c6c
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-19 15:25:41 -0600

    added commenting

--- objects/clock.py
+++ objects/clock.py
@@ -21,11 +21,14 @@ class Clock(Object):
         self.height = height
 
     def draw(self, screen: pygame.surface.Surface) -> None:
+        # get the minutes and seconds since the initial time
         seconds = round(time.time() - self.initial_time)
         minutes, seconds = divmod(seconds, 60)
 
+        # format as string
         displayed_time = f"{minutes:02d}:{seconds:02d}"
 
+        # display the text centered in its position
         width, height = self.font.size(displayed_time)
         font_surf = self.font.render(displayed_time, True, BLACK)
         screen.blit(
